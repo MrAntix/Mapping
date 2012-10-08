51165f5d0871e73a6450740210215b325439dae7
Anthony Johnston <aj@antix.co.uk>
2012-10-08 08:44:17 +0100

    1.0.0-b03

--- antix-mapping/Antix.Mapping/Properties/AssemblyInfo.cs
+++ antix-mapping/Antix.Mapping/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0-b02")]
+[assembly: AssemblyInformationalVersion("1.0.0-b03")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
