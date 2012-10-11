69b43bcdf4aabb0bfcd4be997c42886ec09d2aef
Anthony Johnston <aj@antix.co.uk>
2012-10-11 22:08:51 +0100

    1.0.0-b05
    No work is done on null from in Map functions, this is to allow partial
    graphs to be mapped
    Version updated

--- antix-mapping/Antix.Mapping/Properties/AssemblyInfo.cs
+++ antix-mapping/Antix.Mapping/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0-b04")]
+[assembly: AssemblyInformationalVersion("1.0.0-b05")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
